b2ab55368db7b9094c30d08540842d8831be58b8
Micaelli Medeiros <m.amedeiros@hotmail.com>
2021-06-21 22:58:30 -0300

    chore: Add page new room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -7,6 +9,12 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function Home() {
+  const history = useHistory();
+
+  function handleCreateRoom() {
+    history.push('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -23,7 +31,7 @@ export function Home() {
         <div className="main-content">
           <img src={logoImg} alt="Logo letmeask" />
 
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
