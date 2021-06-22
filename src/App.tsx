534672539653070e9dbd6a8d544353348f5f4e34
Micaelli Medeiros <m.amedeiros@hotmail.com>
2021-06-21 22:58:09 -0300

    chore: Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom' 
+
 import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
 
 function App() {
   return (
-   <Home />
+    <BrowserRouter>
+      <Route path="/" component={Home} exact />
+      <Route path="/rooms/new" component={NewRoom} />
+  </BrowserRouter>
   );
 }
 
